4be6ab0e83a34f0e1907aa7d30403a44e31f2c12
sneurlax <sneurlax@gmail.com>
2016-03-21 16:36:54 -0500

    Pixi.js addition
    Added pixi.js and a dinky little blur effect.
    
    No functionality :) just the way I like it!

--- www/public/index.php
+++ www/public/index.php
@@ -16,13 +16,46 @@ include 'php/colors.php';
 				<td>
 					<table>
 						<tr>
-							<td valign="middle" align="center">
-								<img src="img/fsm.png" />
+							<td valign="middle" align="center" id="stage">
+								<noscript><img src="img/fsm.png" /></noscript>
 							</td>
 						</tr>
 					</table>
 				</td>
 			</tr>
 		</table>
+
+		<script src="vendor/pixi.min.js"></script>
+		<script>
+			var renderer = PIXI.autoDetectRenderer(800, 600, { transparent: true });
+			document.getElementById("stage").appendChild(renderer.view);
+
+			// create the root of the scene graph
+			var stage = new PIXI.Container();
+
+			var FSM = PIXI.Sprite.fromImage('img/fsm.png');
+			FSM.position.x = (renderer.width / 2) - 315;
+			FSM.position.y = 10;
+			stage.addChild(FSM);
+
+			var blur = new PIXI.filters.BlurFilter();
+
+			FSM.filters = [blur];
+
+			var count = 0;
+
+			requestAnimationFrame(animate);
+
+			function animate() {
+
+			    count += 0.005;
+
+			    var blurAmount = Math.cos(count) ;
+
+			    blur.blur = 20 * (blurAmount);
+			    renderer.render(stage);
+			    requestAnimationFrame( animate );
+			}
+		</script>
 	</body>
 </html>
